4d542c16c56e055fc5003f5f3f47f6ce9a50f998
ava-lu <alu21@stuy.edu>
2020-12-13 01:22:04 -0500

    set method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,4 +5,15 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
   }
+  public T set(int index, T value) {
+    if (value==null) {
+      throw new IllegalArgumentException("Cannot set nulls");
+    }
+    for (int i=this.size()-1; i>=0; i--) {
+      if (this.get(i).compareTo(this.get(i-1))==-1) {
+        super.set(i,this.get(i-1));
+      }
+    }
+    return super.set(index,value);
+  }
 }
