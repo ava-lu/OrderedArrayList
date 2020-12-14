a9f870df1093824c7e25c513c80f621b11d060b2
ava-lu <alu21@stuy.edu>
2020-12-13 19:16:32 -0500

    added a helper method but still need to debug

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -9,37 +9,37 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     if (value==null) {
       throw new IllegalArgumentException("Cannot set nulls");
     }
-    for (int i=this.size()-1; i>=0; i--) {
-      if (this.get(i).compareTo(this.get(i-1))==-1) {
-        super.set(i,this.get(i-1));
-      }
-    }
-    return super.set(index,value);
+     // if (index>=super.size() || index<0) {
+     //   throw new IllegalArgumentException("Index not within bounds");
+     // }
+    T ans = super.set(index,value);
+    super.add(this.findindex(value), value);
+    return ans;
   }
   public boolean add(T e) {
     if (e==null) {
       throw new IllegalArgumentException("Cannot add nulls");
     }
-    super.add(e);
-    for (int i=this.size()-1; i>=0; i--) {
-      if (this.get(i).compareTo(this.get(i-1))==-1) {
-        super.set(i,this.get(i-1));
-      }
-    }
+    super.add(this.findindex(e), e);
     return super.add(e);
   }
   public void add(int index, T element) {
     if (element==null) {
       throw new IllegalArgumentException("Cannot add nulls");
     }
-    for (int i=0; i<this.size(); i++) {
-      if (i==index) super.add(i, element);
+    if (index>=super.size() || index<0) {
+      throw new IllegalArgumentException("Index not within bounds");
     }
-    for (int i=this.size()-1; i>=0; i--) {
-      if (this.get(i).compareTo(this.get(i-1))==-1) {
-        super.set(i,this.get(i-1));
+    super.add(this.findindex(element), element);
+  }
+  private int findindex(T element) {
+    int ans=0;
+    for (int i=0; i<super.size(); i++) {
+      if (super.get(i).compareTo(element)==1) {
+        ans = i;
       }
     }
-  }
+    return ans;
+   }
 
 }
