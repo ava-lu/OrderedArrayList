109fae75651e0456c1bc13af22208b234dd65dc8
ava-lu <alu21@stuy.edu>
2020-12-13 19:44:49 -0500

    DEBUGGED

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -9,36 +9,37 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     if (value==null) {
       throw new IllegalArgumentException("Cannot set nulls");
     }
-     // if (index>=super.size() || index<0) {
-     //   throw new IllegalArgumentException("Index not within bounds");
-     // }
-    T ans = super.set(index,value);
-    super.add(this.findindex(value), value);
+    T ans = super.get(index);
+    super.remove(index);
+    super.add(findindex(value), value);
     return ans;
   }
   public boolean add(T e) {
     if (e==null) {
       throw new IllegalArgumentException("Cannot add nulls");
     }
-    super.add(this.findindex(e), e);
-    return super.add(e);
+    add(findindex(e), e);
+    return true;
   }
   public void add(int index, T element) {
     if (element==null) {
       throw new IllegalArgumentException("Cannot add nulls");
     }
-    if (index>=super.size() || index<0) {
-      throw new IllegalArgumentException("Index not within bounds");
-    }
-    super.add(this.findindex(element), element);
+    super.add(findindex(element), element);
   }
   private int findindex(T element) {
     int ans=0;
     for (int i=0; i<super.size(); i++) {
       if (super.get(i).compareTo(element)==1) {
         ans = i;
+        return ans;
+      }
+      else if (super.get(i).compareTo(element)==0) {
+        ans = i;
+        return ans;
       }
     }
+    if (ans==0) ans=super.size();
     return ans;
    }
 
